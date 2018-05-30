aa8edda5971e6c96c61b22e2c9e98e73fca4c041
Mohammed Shakeer Pasha <35602436+shakeer-linux@users.noreply.github.com>
2018-05-30 16:30:34 +0530

    Create myscript.py

---
+++ myscript.py
@@ -0,0 +1,8 @@
+import sys
+uname = sys.argv[1]
+pwd = sys.argv[2]
+
+if   uname=="first" and pwd=="second"  :
+    print  "Welcome to Ansible Automation"
+else :
+    print "Invalid username or password"
